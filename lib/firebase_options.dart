5dd1e7c4544e02e73b4f911fb2af81cee2315850
Dhruvi686 <dhruvipatel2k6@gmail.com>
2026-01-04 22:56:46 +0530

    API key issue resolved

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,8 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     return const FirebaseOptions(
-      apiKey: 'AIzaSyCPdYOKTrpsdDrUkn8hx4nUcGcI24HrJHs',
-      appId: '1:592045169426:android:fecf22010ab0fb2f59b1cb',
+      apiKey: 'abcd1234random',
+      appId: '1:592045169426:android:fecf22010ab0fb2f59b1b',
       messagingSenderId: '789456123098',
       projectId: 'fir-2a526',
     );
